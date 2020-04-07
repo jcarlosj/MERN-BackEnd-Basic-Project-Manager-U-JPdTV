03dda225ffcfe967bd88fc083b100ea1625d1981
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-04-07 17:04:05 -0500

    [+] Express: Launch Server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console .log( 'index.js te saluda! ;)' );
+const 
+    express = require( 'express' ),         // Importa Express
+    app = express(),                        // Asigna Express al inicio de la app
+    PORT = process .env .PORT || 4000;      // Define variable de entorno
+
+/** Define Rutas */
+app .get( '/', ( request, response ) => {
+    response .send( "Project Manager te saluda :)" );
+});
+
+/** Lanza el Servidor */
+app .listen( PORT, error => {
+    if ( error ) throw new Error( error ) ;
+    console .log( `Server run on http://localhost:${ PORT }` );
+});
