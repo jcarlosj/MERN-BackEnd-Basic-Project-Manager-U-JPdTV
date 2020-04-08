e7532b642cec57149ca8a218a7dd66fd0cad1ecb
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-04-08 16:40:06 -0500

    [+] Mongoose: Configure and implement MongoDB (localhost)

--- config/db.js
+++ config/db.js
@@ -4,14 +4,14 @@ require( 'dotenv' ) .config({ path: '.env' });
 const ConnectDatabase = async () => {
     try {
         await mongoose .connect( 
-            process .env .DB_MONGO, 
+            process .env .DB_MONGO_LOCAL, 
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
                 useFindAndModify: false
             }
         );
-        console .log( 'Connected Mongo Atlas database' );
+        console .log( 'Connected MongoDB' );
     } catch ( error ) {
         console .log( error );
         process .exit( 1 );     // Detiene la app
