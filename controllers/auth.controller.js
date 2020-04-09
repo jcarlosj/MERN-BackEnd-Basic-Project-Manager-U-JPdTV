1629490b733fd82ffdd40bc77b66037c8e346063
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-04-09 16:43:30 -0500

    [+] Router & Controller > auth: Validate user password and email to create token with user data

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,71 @@
+const 
+    User = require( '../models/User' ),     // Model
+    /** Dependencies */ 
+    bcrypt = require( 'bcryptjs' ),
+    { validationResult } = require( 'express-validator' ),
+    jwt = require( 'jsonwebtoken' );
+
 exports .authenticateUser = async ( request, response ) => {
     console .log( 'POST /api/auth' );
 
-    response .json({
-        success: true,
-        method: 'POST',
-        path: '/api/auth'
-    });
+    const errors = validationResult( request );     // Implementa validacion a propiedades esperadas
+    
+    /** Verifica si hay Errores de Validacion */
+    if( !errors .isEmpty() ) {
+        return response .status( 400 ) .json({
+            success: false,
+            errors: errors .array()
+        })
+    }
+
+    const { email, password } = request .body;      // Destructuring 
+
+    try {
+        let user = await User .findOne({ email });  // Query using Mongoose
+
+        /** Validate if the user does not exist */
+        if( ! user ) {
+            return response .status( 400 ) .json({
+                success: false,
+                message: 'El usuario NO existe!'
+            });
+        }
+
+        const passwordMatch = await bcrypt .compare( password, user .password );
+
+        /** Validate if the password does not match */
+        if( ! passwordMatch ) {
+            return response .status( 400 ) .json({
+                success: false,
+                message: 'La contraseña es incorrecta.'
+            });
+        }
+
+        /** Data que llevará el Token */
+        const payload = {
+            user: {
+                id: user .id                // ID Registrado en MongoDB
+            }
+        }
+        /** Crea Firma de Verificación del Token */
+        jwt .sign(                         
+            payload,                        // Información
+            process .env .SECRET,           // String
+            {                               // Configuración del Token
+                expiresIn: 3600             // 1 hora
+            },
+            ( error, token ) => {           // CallBack 
+                if( error ) throw error;
+
+                response .json({
+                    success: true,
+                    message: 'Usuario autenticado correctamente!',
+                    token
+                });
+            }
+        );
+
+    } catch ( error ) {
+        
+    }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,16 @@
 const 
     express = require( 'express' ),
     router = express .Router(),
+    { check } = require( 'express-validator' ),                     // Dependency
     authController = require( '../controllers/auth.controller' );   // Controller
 
 /** api/auth */
 router .post( 
-    '/',        // Path 
+    '/',        // Path
+    [           // Validation of express-validator fields
+        check( 'email', 'Debe ser valido' ) .isEmail(),
+        check( 'password', 'Longitud mínima 6 caracteres' ) .isLength({ min: 6 })
+    ], 
     authController .authenticateUser // Run controller functionality
 );
 
