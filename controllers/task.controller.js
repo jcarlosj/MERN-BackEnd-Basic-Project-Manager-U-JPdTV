6b1f59d81321e843f675b5d1690cc5057cd1e747
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-04-13 16:49:16 -0500

    [+] Controller > task: Create authenticated user tasks

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,3 +1,70 @@
+const 
+    /** Models */
+    Project = require( '../models/Project' ),       
+    Task = require( '../models/Task' ),       
+    { validationResult } = require( 'express-validator' );  // Dependency
+
+/** Crea un nuevo proyecto */
+exports .create = async ( request, response ) => {
+    console .log( 'POST /api/tasks');
+
+    const errors = validationResult( request );     // Implementa validacion a propiedades esperadas
+    
+    /** Verifica si hay Errores de Validacion */
+    if( !errors .isEmpty() ) {
+        return response .status( 400 ) .json({
+            success: false,
+            errors: errors .array()
+        });
+    }
+
+    const { project } = request .body;          // Destructuring request .body
+
+    try {
+        
+        const projectDB = await Project .findById( project );     // Busca proyecto por ID
+
+        /** Valida si NO existe el proyecto */
+        if( ! projectDB ) {
+            return response .status( 404 ) .json({
+                success: false,
+                error: {
+                    message: 'El proyecto no ha sido encontrado!'
+                }
+            });
+        }
+
+        /** Valida si NO coincide el usuario creador del proyecto */
+        if( projectDB .createBy .toString() !== request .user .id ) {
+            return response .status( 404 ) .json({
+                success: false,
+                error: {
+                    message: 'Usuario no autorizado! No coincide con el creador del proyecto.'
+                }
+            });
+        }
+
+        const task = new Task( request .body );   // Crea 'tarea' usando el Modelo e inserta los datos.
+
+        await task .save();    // Registra los datos el proyecto en MongoDB usando Mongoose.
+
+        response .json({
+            success: true,
+            message: 'Tarea registrada correctamente!',
+            task
+        });
+
+    } catch ( error ) {
+        console .log( error );
+        response .status( 500 ) .json({
+            success: false,
+            error: {
+                message: 'La tarea no ha sido registrada!'
+            }
+        });
+    }
+}
+
 /** Obtiene todas las tareas del proyecto seleccionado */
 exports .getAll = ( request, response ) => {
     console .log( 'GET /api/tasks');
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,22 @@
 const 
     express = require( 'express' ),
     router = express .Router(),
-    taskController = require( '../controllers/task.controller' );     // Controller
+    taskController = require( '../controllers/task.controller' ),     // Controller
+    auth = require( '../middlewares/auth' ),      // Middleware
+    { check } = require( 'express-validator' );   // Dependency
+
+/** PATH: api/tasks */
+
+/** Nueva tarea */
+router .post( 
+    '/',        // Path
+    [ auth ],   // Middleware de autenticación
+    [           // Validation of express-validator fields
+        check( 'project', 'Nombre del proyecto es obligatorio' ) .not() .isEmpty(),
+        check( 'name', 'Nombre de la tarea es obligatorio' ) .not() .isEmpty()
+    ],
+    taskController .create   // Run controller functionality
+);
 
 /** Obtiene todos los proyectos */
 router .get(
