132cd293e66a31c663ed823b63f47a285beed0b5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-04-16 16:44:29 -0500

    [+] NPM > cors: Install & Implement Middleware

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const
     express = require( 'express' ),         // Importa Express
     app = express(),                        // Asigna Express al inicio de la app
     PORT = process .env .PORT || 4000,      // Define variable de entorno
-    ConnectDatabase = require( './config/db' ); // Configuración Conexión Base de Datos Mongo Atlas
+    ConnectDatabase = require( './config/db' ), // Configuración Conexión Base de Datos Mongo Atlas
+    cors = require( 'cors' );               // Permite Cross-Origin Resource Sharing
 
 /** Middlewares */
+app .use( cors() );     // Habilita transferencias de datos entre navegadores y servidores.
 app .use( express .json({ extended: true }));           // Habilita uso de JSON (Obliga enviar 'Content-Type':'application/json' el header de la petición)
 app .use( '/api/users', require( './routes/users' ) );  // Define Rutas para API
 app .use( '/api/auth', require( './routes/auth' ) );
